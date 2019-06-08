02577b7bc07b48f8ed61224281d865f0bae5d056
Ryo <Gegegegensan@users.noreply.github.com>
2019-06-08 20:19:08 +0800

    Add files

---
+++ app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import *
+from feedparser import parse
+import feedparser
+from datetime import datetime as dt
+from urllib.parse import urlencode
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+def parseDate(dateData):
+    return dt(
+        dateData.tm_year,
+        dateData.tm_mon,
+        dateData.tm_mday,
+        dateData.tm_hour,
+        dateData.tm_min,
+        dateData.tm_sec
+    )
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+	urls = [
+		    'blog feed URL',
+		    'blog feed URL',
+		    'blog feed URL',
+		    'blog feed URL',
+		]
+	entries = [
+	    {
+	        'title': entry['title'],
+	        'link': entry['link'],
+	        'date': parseDate(entry['updated_parsed'] or entry['published_parsed'])
+	    }
+		for url in urls
+	    for entry in parse(url).entries
+	]
+	entries.sort(key=lambda x: x['date'], reverse=True)
+	return render_template('index.html',  entries=entries)
+
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
